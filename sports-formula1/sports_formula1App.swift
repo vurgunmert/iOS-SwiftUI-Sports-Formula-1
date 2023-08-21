ddf52eb179fb7c343562b1e010afb2639ac61ec0
Mert Vurgun <vurgun@medienmonster.com>
2023-08-21 12:09:11 +0300

    Initial commit

---
+++ sports-formula1/sports_formula1App.swift
@@ -0,0 +1,17 @@
+//
+//  sports_formula1App.swift
+//  sports-formula1
+//
+//  Created by Mert Vurgun on 19.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct sports_formula1App: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
